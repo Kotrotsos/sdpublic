8690fb656cf1b30a520179e0610a1e38e7c36774
M.K. Kotrotsos <m.k.kotrotsos@MKs-iMac.local>
2022-08-27 18:30:01 +0200

    added options

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,30 @@ stability_api = client.StabilityInference(
 
 # st.code(st.secrets["key"])
 
+option = st.sidebar.selectbox(
+     'Want to use a template?',
+     ('', 'Synthwave car', 'HR Gigeresque landscape', 'Van Gogh painting'))
+
+st.write('You selected:', option)
+
+if option == 'Synthwave car':
+    template = "Car Synthwave Highly detailed, surrealism, trending on art station, triadic color scheme, smooth, sharp focus, matte, elegant, the most beautiful image ever seen, illustration, digital paint, dark, gloomy, octane render, 8k, 4k, washed colors, sharp, dramatic lighting, beautiful, post processing, picture of the day, ambient lighting, epic composition"
+elif option == 'HR Gigeresque landscape':
+    template = "HR Giger, Alien landscape. surrealism. Dark. High Quality. Ultra Realistic."
+elif option == 'Van Gogh painting':
+    template = "Painting by Vincent Van Gogh"
+else:
+    template = ""
+
+
+
 options = st.sidebar.multiselect('Assemble a description', ["Car", "Beautiful girl", "Landscape", "River", "A man", "Skyline", "Highly detailed","surrealism","trending on art station","triadic color scheme","smooth","sharp focus","matte","elegant","the most beautiful image ever seen","illustration","digital paint","dark","gloomy","octane render","8k","4k","washed colors","sharp","dramatic lighting","beautiful","post processing","picture of the day","ambient lighting","epic composition"])
 
-txt = st.sidebar.text_area('Or write out the description yourself.',' '.join(options),placeholder='A River flows into a lake. High Quality Art.')
+if template == "":
+    txt = st.sidebar.text_area('Or write out the description yourself.',', '.join(options),placeholder='A River flows into a lake. High Quality Art.')
+else:
+     txt = st.sidebar.text_area('Or write out the description yourself.',template,placeholder='A River flows into a lake. High Quality Art.')
+
 
 if st.sidebar.button('Generate image from this description'):
     answers = stability_api.generate(
